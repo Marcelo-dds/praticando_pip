a00bf131169b83f5d880b85dccbf2f540ee6133a
marcelo-dds <marcelodds_web@hotmail.com>
2025-05-21 21:36:37 -0300

    usando uma forma de keyboad para fins didaticos

---
+++ keyboardpratica.py
@@ -0,0 +1,9 @@
+import keyboard
+
+keyboard.press_and_release("ctrl+t")
+
+keyboard.write('https://www.google.com/')
+
+keyboard.press_and_release("enter")
+
+""" codigo nao muito confiavel usado com keyboard que faz com que a janela que esta no fogo use os comandos utilizado no codigo """
