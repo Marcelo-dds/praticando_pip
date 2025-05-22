eafe5b42df86b60b12476c955c1c1c08774c19d5
marcelo-dds <marcelodds_web@hotmail.com>
2025-05-21 21:42:42 -0300

    ajustado de maneira que n precisa do foco

--- keyboardpratica.py
+++ keyboardpratica.py
@@ -1,9 +1,25 @@
 import keyboard
+import time
 
+# Pressiona tecla Win para abrir o menu iniciar
+keyboard.press_and_release('win')
+time.sleep(0.5)
+
+# Digita "chrome"
+keyboard.write('chrome')
+time.sleep(0.5)
+
+# Pressiona Enter para abrir o Chrome
+keyboard.press_and_release('enter')
+time.sleep(2)  # Aguarda o Chrome abrir
+
+# Pressiona Ctrl+T para abrir nova aba
 keyboard.press_and_release("ctrl+t")
+time.sleep(0.5)
 
+# Digita a URL
 keyboard.write('https://www.google.com/')
+time.sleep(0.2)
 
+# Pressiona Enter
 keyboard.press_and_release("enter")
-
-""" codigo nao muito confiavel usado com keyboard que faz com que a janela que esta no fogo use os comandos utilizado no codigo """
